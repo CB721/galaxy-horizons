293567d5642e1441344a463ae0612547519c5e02
Clint Brodar <clintbrodar@D-10-18-184-241.dhcp4.washington.edu>
2019-07-16 20:47:45 -0700

    Initial files

---
+++ Assets/Scripts/buttonOptions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonOptions : MonoBehaviour
+{
+    public void playGame () {
+        SceneManager.LoadScene(1);
+    }
+    public void mainMenu () {
+        SceneManager.LoadScene(0);
+    }
+}
